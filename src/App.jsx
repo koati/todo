0012e34b9c923269529c97adfc3543301f75d869
Kovács Attila <koati.k9@gmail.com>
2021-11-24 21:10:49 +0100

    drag-drop

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import './App.css';
+import React from 'react';
+import Dashboard from './Components/Dashboard';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+
+const nextId = arr => arr.reduce((acc, obj)=>Math.max(acc, obj.id), 0) + 1
+
+const demoDashboards= [
+    {id: 1, name: "Bevásárlás" },
+    {id: 2, name: "Tanulás" },
+  ]
+
+const demoTodos = [
+    { id: 2, dashId: 1, name: "Lidl", description: "Tej, kenyér" },
+    { id: 3, dashId: 1, name: "Decathlon", description: "Kosárlabda" },
+    { id: 4, dashId: 1, name: "Media Markt", description: "Laptop" },
+    { id: 6, dashId: 2, name: "Codecool", description: "React, jest" },
+    { id: 7, dashId: 2, name: "Egyetem", description: "Vezetésszervezés" },
+]
+
+const App = () => {
+    const [dashboards, setDashboards] = React.useState(demoDashboards)
+    const [todos, setTodos] = React.useState(demoTodos)
+
+    const addDashboard = () => setDashboards([...dashboards, {id: nextId(dashboards), name: 'Untilted dashboard'}])
+    const addTodo = (id) => setTodos([...todos, {id: nextId(todos), dashId: id}])
+    const removeTodo = (id) => setTodos(todos.filter(todo=>todo.id !== id))
+    const renameDashboard = (id, name) => {
+        const newArr = [...dashboards]
+        const obj = newArr.find(item=>item.id===id)
+        obj.name = name
+        setDashboards(newArr)
+    }
+    const modifyTodo = (id, name, description) => {
+        const newArr = [...todos]
+        const obj = newArr.find(item=>item.id===id)
+        obj.name = name
+        obj.description = description
+        setTodos(newArr)
+    }
+    const functions = { addDashboard, addTodo, removeTodo, renameDashboard, modifyTodo }
+
+    function handleOnDragEnd(result) {
+        const newArr = [...todos]
+        const obj = newArr.find(item=>item.id===result.draggableId*1)
+        obj.dashId = result.destination.droppableId*1
+        setTodos(newArr)
+    }
+
+    return (
+        <div id="app-container">
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+            {dashboards.map(dashboard => <Dashboard key={dashboard.id} dashboard={dashboard} todos={todos.filter(todo=>todo.dashId===dashboard.id)} {...functions} />)}
+            </DragDropContext>
+            <button onClick={addDashboard}>+</button>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Card = ({card, removeTodo, modifyTodo}) => {
+    const [name, setName] = React.useState(card.name)
+    const [desc, setDesc] = React.useState(card.description)
+    const [edit, setEdit] = React.useState(false)
+    const [visible, setVisible] = React.useState(true)
+
+    const save = () => {
+        modifyTodo(card.id, name, desc)
+        setEdit(false)
+    }
+    const cancel = () => {
+        setName(card.name)
+        setDesc(card.desc)
+        setEdit(false)
+    }
+
+    return (
+        <div className="card" onClick={(e)=> {if (e.target === e.currentTarget) setVisible(!visible)} } >
+            {edit && <input value={name} onChange={(e)=>setName(e.target.value)} placeholder='Type a name' />}
+            {!edit && <h3>{card.name}</h3>}
+
+            {edit && visible && <textarea value={desc} onChange={(e)=>setDesc(e.target.value)} cols="30" rows="10"  placeholder='Type the description' />}
+            {!edit && visible && <p>{card.description}</p>}
+
+            <div className="buttonbar">
+            {!edit && <button onClick={()=>setEdit(true)}>Edit</button>}
+            {edit && <button onClick={cancel}>Cancel</button>}
+            {edit && <button onClick={save}>Save</button>}
+            <button onClick={()=>removeTodo(card.id)}>Delete card</button>
+            </div>
+        </div>
+    )
+}
+
+export default Card
